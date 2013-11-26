beb6346fd47fb64ca07ce801b7864783bd9615d6
Nick Phillips <nick.phillips@otago.ac.nz>
2013-11-26 16:21:47 +1300

    Remove and re-add OU editor/supsub module in order to update -- to avoid confusion with cherry-pick of equivalent 2.4 commit    leaving uncommitted directory.

--- lib/editor/supsub/version.php
+++ lib/editor/supsub/version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012021000;
-$plugin->requires  = 2010112400;
+$plugin->version   = 2013072500;
+$plugin->requires  = 2012120300;
 $plugin->cron      = 0;
 $plugin->component = 'editor_supsub';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0';
+$plugin->release   = '1.4 for Moodle 2.4+';
 
 $plugin->dependencies = array('editor_tinymce' => ANY_VERSION);
