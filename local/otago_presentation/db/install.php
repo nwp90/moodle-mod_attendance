1616820881959bb11d5cb717219981fbe08c07af
Peter Bulmer <peter.bulmer@catalyst.net.nz>
2013-11-27 22:26:45 +1300

    local/otago - enable webservices during install

--- local/otago_presentation/db/install.php
+++ local/otago_presentation/db/install.php
@@ -1,5 +1,23 @@
 <?php
 
 function xmldb_local_otago_presentation_install() {
-}
+    global $DB, $CFG;
+    $systemcontext = get_system_context();
 
+    // Admin has just installed our module.
+    // To be at all useful, we need webservices, and rest in particular.
+    // Assert that these are enabed for the admin.
+    if (empty($CFG->enablewebservices)) {
+        set_config('enablewebservices', '1');
+    }
+    if (empty($CFG->webserviceprotocols)) {
+        $webserviceprotocols = array();
+    } else {
+        $webserviceprotocols = explode(',', $CFG->webserviceprotocols);
+    }
+    if (!in_array('xmlrpc', $webserviceprotocols)) {
+        $webserviceprotocols[] = 'rest';
+        $protocollist = implode(',', $webserviceprotocols);
+        set_config('webserviceprotocols', $protocollist);
+    }
+}
