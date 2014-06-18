54dfc5a0cf9144fa35128a83ebd5401dc4611556
Nick Phillips <nick.phillips@otago.ac.nz>
2014-06-19 10:54:38 +1200

    Remove bootstrap from jQuery plugins.php

--- lib/jquery/plugins.php
+++ lib/jquery/plugins.php
@@ -43,5 +43,4 @@ $plugins = array(
     'ui-css'  => array('files' => array('ui-1.11.4/theme/smoothness/jquery-ui.min.css')),
     'expander'=> array('files' => array('expander-1.4.7/jquery.expander.min.js')),
     'rwdimagemaps' => array('files' => array('jquery.rwdImageMaps-20130912.min.js')),
-    'bootstrap-js' => array('files' => array('bootstrap-2.3.2.min.js')),
 );
