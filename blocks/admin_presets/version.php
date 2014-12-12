c1edf40cc0c16be4473fd9254e92a352b575d719
Nick Phillips <nick.phillips@otago.ac.nz>
2014-12-12 13:57:48 +1300

    Update admin_presets to 27.1 (Build: 2014072201) from https://moodle.org/plugins/pluginversions.php?plugin=block_admin_presets

--- blocks/admin_presets/version.php
+++ blocks/admin_presets/version.php
@@ -3,8 +3,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014072201;
-$plugin->requires = 2013110500;
+$plugin->requires = 2014050800;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '26.2 (Build: 2014072201)';
+$plugin->release = '27.1 (Build: 2014072201)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
