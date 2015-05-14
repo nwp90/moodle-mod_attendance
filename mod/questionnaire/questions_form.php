8b2a3fd5ae8dc0a3a67d4ab3c565d9062220a797
Nick Phillips <nick.phillips@otago.ac.nz>
2015-05-15 09:42:22 +1200

    Update mod questionnaire to version 2014111002 / build 2015031901

--- mod/questionnaire/questions_form.php
+++ mod/questionnaire/questions_form.php
@@ -60,13 +60,14 @@ class questionnaire_questions_form extends moodleform {
         if (!($qtypes = $DB->get_records_select_menu('questionnaire_question_type', $select, null, '', 'typeid,type'))) {
             $qtypes = array();
         }
-        // Needed for non-English languages.
+        // Get the names of each question type in the appropriate language.
         foreach ($qtypes as $key => $qtype) {
             // Do not allow "Page Break" to be selected as first element of a Questionnaire. 
-            if (empty($questionnaire->questions) && $qtype == 'Page Break') {
-                continue;
+            if (empty($questionnaire->questions) && ($qtype == 'Page Break')) {
+                unset($qtypes[$key]);
+            } else {
+                $qtypes[$key] = questionnaire_get_type($key);
             }
-            $qtypes[$key] = questionnaire_get_type($key);
         }
         natsort($qtypes);
         $addqgroup = array();
@@ -547,7 +548,7 @@ class questionnaire_edit_question_form extends moodleform {
                 $choices[$lines] = get_string('nlines', 'questionnaire', $lines);
             }
             $mform->addElement('select', 'length', get_string('responsefieldlines', 'questionnaire'), $choices);
-        } else if ($question->type_id == QUESCHECK || $question->type_id == QUESNUMERIC) {
+        } else if ($question->type_id == QUESCHECK || $question->type_id == QUESNUMERIC || $question->type_id == QUESTEXT) {
             $question->precise = isset($question->precise) ? $question->precise : $defprecise;
             $mform->addElement('text', 'precise', get_string($phelpname, 'questionnaire'), array('size' => '1'));
         }
