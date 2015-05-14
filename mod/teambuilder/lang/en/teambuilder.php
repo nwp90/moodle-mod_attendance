5e255c440bd848dc0f053db2395c4c0ddce396cf
Nick Phillips <nick.phillips@otago.ac.nz>
2015-05-15 09:02:40 +1200

    Add UNSW teambuilder module "2014082601" (actually 2015050400) for 2.7, 2.8.

---
+++ mod/teambuilder/lang/en/teambuilder.php
@@ -0,0 +1,20 @@
+<?php
+$string['buildteams'] = 'Build Teams';
+
+$string['intro'] = 'Introduction';
+$string['intro_help'] = 'The introduction to your Team Builder instance.';
+
+$string['modulename'] = 'Team Builder';
+$string['modulenameplural'] = 'Team Builders';
+$string['name'] = 'Name';
+$string['pluginadministration'] = 'Team Builder administration';
+$string['pluginname'] = 'Team Builder';
+
+$string['preview'] = 'Preview';
+$string['questionnaire'] = 'Questionnaire';
+
+$string['teambuilder'] = 'Team Builder';
+$string['teambuilder:addinstance'] = 'Add a new teambuilder module';
+$string['teambuilder:build'] = 'Build teams from survey response';
+$string['teambuilder:create'] = 'Create survey';
+$string['teambuilder:respond'] = 'Respond to Questionnaire';
