dc15348e699b7d9629e8d818f6be024477a4f9cd
Nick Phillips <nick.phillips@otago.ac.nz>
2015-07-07 12:21:13 +1200

    Add atto styles plugin built 2015052700 'v2014111003', from https://moodle.org/plugins/view/atto_styles

---
+++ lib/editor/atto/plugins/styles/version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto styles - Version file
+ *
+ * @package     atto
+ * @subpackage  atto_styles
+ * @copyright   2015 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>, together with Synergy Learning UK
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'atto_styles';
+$plugin->version = 2014111003;
+$plugin->release = '2.8 (Build: 2015052700)';
+$plugin->requires = 2014111000;
+$plugin->maturity = MATURITY_STABLE;
