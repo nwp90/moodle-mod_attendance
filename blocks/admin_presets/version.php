866babbadb584e9d9502fd2f919cbb02f22db574
Nick Phillips <nick.phillips@otago.ac.nz>
2015-11-18 15:02:51 +1300

    Update admin_presets block (rm, re-add)

--- blocks/admin_presets/version.php
+++ blocks/admin_presets/version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014072201;
-$plugin->requires = 2014050800;        // Requires this Moodle version
+$plugin->version = 2015050500;
+$plugin->requires = 2015050500;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '27.1 (Build: 2014072201)';
+$plugin->release = '1.29.0 (Build: 2015050500)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
