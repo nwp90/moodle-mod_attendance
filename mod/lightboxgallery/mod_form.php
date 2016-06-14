348756391eb1f75194136b9b0b98122852470d37
Nick Phillips <nick.phillips@otago.ac.nz>
2016-06-14 14:59:20 +1200

    Update mod_lightboxgallery to 2016052400 release.

--- mod/lightboxgallery/mod_form.php
+++ mod/lightboxgallery/mod_form.php
@@ -61,9 +61,6 @@ class mod_lightboxgallery_mod_form extends moodleform_mod {
         $mform->addElement('select', 'perpage', get_string('imagesperpage', 'lightboxgallery'), $this->get_perpage_options());
         $mform->setType('perpage', PARAM_INTEGER);
 
-        $mform->addElement('select', 'perrow', get_string('imagesperrow', 'lightboxgallery'), $this->get_perrow_options());
-        $mform->setType('perrow', PARAM_INTEGER);
-
         $yesno = array(0 => get_string('no'), 1 => get_string('yes'));
 
         $mform->addElement('select', 'captionfull', get_string('captionfull', 'lightboxgallery'), $yesno);
@@ -75,17 +72,19 @@ class mod_lightboxgallery_mod_form extends moodleform_mod {
         );
         $mform->addElement('select', 'captionpos', get_string('captionpos', 'lightboxgallery'), $captionposopts);
 
-        $autoresizegroup = array();
-        $autoresizegroup[] = &$mform->createElement('select', 'autoresize', get_string('autoresize', 'lightboxgallery'),
+        $autoresize = $mform->createElement('select', 'autoresize', get_string('autoresize', 'lightboxgallery'),
                                 $this->get_autoresize_options());
-        $autoresizegroup[] = &$mform->createElement('checkbox', 'autoresizedisabled', null, get_string('disable'));
+        $autoresizegroup = array();
+        $autoresizegroup[] = $mform->createElement('select', 'autoresize', get_string('autoresize', 'lightboxgallery'),
+                                $this->get_autoresize_options());
+        $autoresizegroup[] = $mform->createElement('checkbox', 'autoresizedisabled', null, get_string('disable'));
         $mform->addGroup($autoresizegroup, 'autoresizegroup', get_string('autoresize', 'lightboxgallery'), ' ', false);
         $mform->setType('autoresize', PARAM_INTEGER);
         $mform->disabledIf('autoresizegroup', 'autoresizedisabled', 'checked');
         $mform->addHelpButton('autoresizegroup', 'autoresize', 'lightboxgallery');
 
         $mform->addElement('select', 'resize', sprintf('%s (%s)', get_string('edit_resize', 'lightboxgallery'),
-                            strtolower(get_string('upload'))), lightboxgallery_resize_options());
+                            core_text::strtolower(get_string('upload'))), lightboxgallery_resize_options());
         $mform->setType('resize', PARAM_INTEGER);
         $mform->disabledIf('resize', 'autoresize', 'eq', 1);
         $mform->disabledIf('resize', 'autoresizedisabled', 'checked');
@@ -119,7 +118,9 @@ class mod_lightboxgallery_mod_form extends moodleform_mod {
     }
 
     public function data_preprocessing(&$defaults) {
-        $defaults['autoresizedisabled'] = (isset($defaults['autoresize']) && $defaults['autoresize'] ? 0 : 1);
+        if (!isset($this->current->add)) {
+            $defaults['autoresizedisabled'] = isset($defaults['autoresize']) && $defaults['autoresize'] ? 0 : 1;
+        }
     }
 
     // Custom functions.
@@ -127,14 +128,6 @@ class mod_lightboxgallery_mod_form extends moodleform_mod {
     private function get_perpage_options() {
         $perpages = array(10, 25, 50, 100, 200);
         $result = array(0 => get_string('showall', 'lightboxgallery'));
-        foreach ($perpages as $perpage) {
-            $result[$perpage] = $perpage;
-        }
-        return $result;
-    }
-
-    private function get_perrow_options() {
-        $perpages = array(2, 3, 4, 5, 6);
         foreach ($perpages as $perpage) {
             $result[$perpage] = $perpage;
         }
