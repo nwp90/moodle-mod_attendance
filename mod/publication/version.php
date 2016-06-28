07a2112d2fcfb12ebe49aff501618bc654ea1813
Nick Phillips <nick.phillips@otago.ac.nz>
2016-06-28 15:33:36 +1200

    Add mod_publication 2016051100 (release).

---
+++ mod/publication/version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of mod_publication for Moodle - http://moodle.org/
+//
+// It is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// It is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * version.php
+ *
+ * @package       mod_publication
+ * @author        Andreas Hruska (andreas.hruska@tuwien.ac.at)
+ * @author        Katarzyna Potocka (katarzyna.potocka@tuwien.ac.at)
+ * @author        Philipp Hager (office@phager.at)
+ * @author        Andreas Windbichler
+ * @copyright     2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->version  = 2016051100;
+$plugin->release   = "v3.0.0";          // User-friendly version number.
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->requires = 2015111600;         // Requires this Moodle version.
+$plugin->component = 'mod_publication'; // Full name of the plugin (used for diagnostics).
