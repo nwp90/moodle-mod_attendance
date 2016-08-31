4692ec2a6229ac20dc4acb0bee06b8ff4c10385e
Nick Phillips <nick.phillips@otago.ac.nz>
2016-08-31 15:55:42 +1200

    format_topcoll: upgrade to upstream 3.1.1.2 (2016052302)

--- course/format/topcoll/version.php
+++ course/format/topcoll/version.php
@@ -34,8 +34,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015103005;
+$plugin->version = 2016052302;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires  = 2015111600.00; // 3.0 (Build: 20151116).
+$plugin->requires  = 2016052300.00; // 3.1 (Build: 20160523).
 $plugin->component = 'format_topcoll';
-$plugin->release = '3.0.2.1';
+$plugin->release = '3.1.1.2';
