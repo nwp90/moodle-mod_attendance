b782a28f62051a6bcf3b3a6c7b612abba204d4ae
Nick Phillips <nick.phillips@otago.ac.nz>
2017-01-24 15:55:52 +1300

    Update atto plugins count, fullscreen, hr, morefontcolors, styles.

--- lib/editor/atto/plugins/styles/version.php
+++ lib/editor/atto/plugins/styles/version.php
@@ -18,14 +18,14 @@
  * Atto styles - Version file
  *
  * @package    atto_styles
- * @copyright  2015 Andrew Davidson, Synergy Learning UK <andrew.davidson@synergy-learning.com> on behalf of Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @copyright  2015 Andrew Davidson, Synergy Learning UK <andrew.davidson@synergy-learning.com> on behalf of Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_styles';
-$plugin->version = 2016071900;
-$plugin->release = 'v3.1-r1';
-$plugin->requires = 2016052300;
+$plugin->version = 2017011800;
+$plugin->release = 'v3.2-r1';
+$plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
