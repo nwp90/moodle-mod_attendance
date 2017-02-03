3e2a44a0f66375e7a683f3e34feec2a5167e8a1d
Nick Phillips <nick.phillips@otago.ac.nz>
2017-02-03 16:35:17 +1300

    Fix bug in Bootstrap as included in bootstrapbase theme, causing dropdown buttons to fail to work when there is a dropdown-toggle with "href='#'" somewhere in the page.

--- theme/bootstrapbase/amd/src/bootstrap.js
+++ theme/bootstrapbase/amd/src/bootstrap.js
@@ -756,7 +756,8 @@ define(['jquery'], function($) {
       selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
     }
 
-    $parent = selector && $(selector)
+    $parent = selector && selector != '#' && $(selector)
+//    $parent = selector && $(selector)
 
     if (!$parent || !$parent.length) $parent = $this.parent()
 
