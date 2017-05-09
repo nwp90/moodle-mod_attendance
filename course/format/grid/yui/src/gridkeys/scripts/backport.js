dc08f0bd88fecd7ecd4cacfedb54e37ac777f511
Nick Phillips <nick.phillips@otago.ac.nz>
2017-05-09 14:16:52 +1200

    Update format_grid to 20170509-git (r3.2.1.1 plus one fix)

--- course/format/grid/yui/src/gridkeys/scripts/backport.js
+++ course/format/grid/yui/src/gridkeys/scripts/backport.js
@@ -28,7 +28,8 @@ var targetfile = path.resolve(targetdir, modname + '.js');
 var inputfile   = fs.createReadStream(sourcefile),
     outputfile  = fs.createWriteStream(targetfile);
 
-inputfile.pipe(outputfile, function() {
+inputfile.pipe(outputfile);
+inputfile.on("end", function() {
   console.log("Copied " + sourcefile + " to " + targetfile);
   process.exit(0);
 });
