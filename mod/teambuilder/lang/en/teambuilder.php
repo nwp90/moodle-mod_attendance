b805bb0b31f96f37dc6f13203a10dac5eb55af11
Nick Phillips <nick.phillips@otago.ac.nz>
2017-06-07 14:58:42 +1200

    Update mod_teambuilder to 2017051900

--- mod/teambuilder/lang/en/teambuilder.php
+++ mod/teambuilder/lang/en/teambuilder.php
@@ -1,20 +1,70 @@
 <?php
-$string['buildteams'] = 'Build Teams';
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Lang strings for the Teambuilder module.
+ *
+ * @package    mod_teambuilder
+ * @copyright  UNSW
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addnewcriterion'] = 'Add New Criterion';
+$string['addanewquestion'] = 'Add a new question';
+$string['addnewquestion'] = 'Add New Question';
+$string['addtogrouping'] = 'Add To Grouping';
+$string['answertype'] = 'Answer type';
+$string['answers'] = 'Answers';
+$string['assignrandomly'] = 'Assign Randomly';
+$string['buildteams'] = 'Build Teams';
+$string['confirmgroupbuilding'] = 'Are you sure you want to create your groups now? This action cannot be undone.';
+$string['creategroups'] = 'Create Groups';
+$string['dontassigngrouptogrouping'] = 'Don\'t assign groups to a Grouping';
+$string['importquestionsfrom'] = 'Import questions from';
 $string['intro'] = 'Introduction';
 $string['intro_help'] = 'The introduction to your Team Builder instance.';
-
 $string['modulename'] = 'Team Builder';
 $string['modulenameplural'] = 'Team Builders';
+$string['modulename_help'] = 'Teambuilder is a tool to assign students to Moodle groups based on their answers to a series of questions that you specify.
+The idea is to:
+
+* define the different roles to be performed within the group activity
+* formulate multiple-choice questions so as to identify the students best suited to filling those roles, then
+* the tool will distribute those students evenly among a specified number of teams.';
 $string['name'] = 'Name';
+$string['noeditingafteropentime'] = 'You cannot edit the questionnaire of a Team Builder if it has already been opened.';
+$string['noneedtocomplete'] = 'You do not need to complete this Team Builder questionnaire.';
+$string['notopen'] = 'This Team Builder questionnaire is not open.';
+$string['numberofteams'] = 'Number of teams';
 $string['pluginadministration'] = 'Team Builder administration';
 $string['pluginname'] = 'Team Builder';
-
+$string['prefixteamnames'] = 'Prefix Team Names with Grouping Name';
+$string['prioritize'] = 'Prioritize';
+$string['prioritizeequal'] = 'equal team numbers';
+$string['prioritizemostcriteria'] = 'most criteria met';
 $string['preview'] = 'Preview';
 $string['questionnaire'] = 'Questionnaire';
-
+$string['resetteams'] = 'Reset Teams';
+$string['savequestionnaire'] = 'Save Questionnaire';
+$string['selectone'] = 'Select one';
+$string['selectany'] = 'Select any (or none)';
+$string['selectatleastone'] = 'Select one or more';
 $string['teambuilder'] = 'Team Builder';
 $string['teambuilder:addinstance'] = 'Add a new teambuilder module';
 $string['teambuilder:build'] = 'Build teams from survey response';
 $string['teambuilder:create'] = 'Create survey';
 $string['teambuilder:respond'] = 'Respond to Questionnaire';
+$string['unassignedtoteams'] = 'Unassigned to teams';
--- mod/teambuilder/view.php
+++ mod/teambuilder/view.php
@@ -14,30 +14,29 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/../../config.php');
 require_once(dirname(__FILE__).'/lib.php');
 
-$PAGE->requires->js("/mod/teambuilder/js/jquery.js");
-$PAGE->requires->js("/mod/teambuilder/js/jquery.ui.js");
+$PAGE->requires->jquery();
+$PAGE->requires->jquery_plugin('ui');
+$PAGE->requires->jquery_plugin('ui-css');
 $PAGE->requires->js("/mod/teambuilder/js/json2.js");
 $PAGE->requires->css('/mod/teambuilder/styles.css');
 
-$id = optional_param('id', 0, PARAM_INT); // course_module ID, or
-$a  = optional_param('a', 0, PARAM_INT);  // teambuilder instance ID
+$id = optional_param('id', 0, PARAM_INT); // The course_module ID, or...
+$a  = optional_param('a', 0, PARAM_INT);  // Teambuilder instance ID.
 $preview = optional_param('preview', 0, PARAM_INT);
 $action = optional_param('action', null, PARAM_TEXT);
 
 if ($id) {
-    $cm = get_coursemodule_from_id('teambuilder', $id, 0, false, MUST_EXIST);
-    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    list ($course, $cm) = get_course_and_cm_from_cmid($id, 'teambuilder');
     $teambuilder = $DB->get_record('teambuilder', array('id' => $cm->instance), '*', MUST_EXIST);
-} else if ($a) {
-    $teambuilder = $DB->get_record('teambuilder', array('id' => $a), '*', MUST_EXIST);
-    $cm = get_coursemodule_from_instance('teambuilder', $teambuilder->id, 0, false, MUST_EXIST);
-    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $id = $cm->id;
 } else {
-    print_error('You must specify a course_module ID or an instance ID');
+    if (!$teambuilder = $DB->get_record('teambuilder', array('id' => $a), '*', MUST_EXIST)) {
+        print_error('You must specify a course_module ID or an instance ID');
+    }
+    list ($course, $cm) = get_course_and_cm_from_instance($teambuilder, 'teambuilder');
+    $id = $cm->id;
 }
 
 require_login($course, true, $cm);
@@ -57,49 +56,43 @@ $event->trigger();
 // Check out if we've got any submitted data.
 
 if ($action == "submit-questionnaire") {
-    $questions = teambuilder_get_questions($teambuilder->id,$USER->id);
-    if(has_capability('mod/teambuilder:respond', $ctxt)) {
-        foreach($questions as $q) {
+    $questions = teambuilder_get_questions($teambuilder->id, $USER->id);
+    if (has_capability('mod/teambuilder:respond', $ctxt)) {
+        foreach ($questions as $q) {
             if ($q->type === 'one') {
                 $response = optional_param('question-'.$q->id, 0, PARAM_RAW);
             } else {
                 $response = optional_param_array('question-'.$q->id, 0, PARAM_RAW);
             }
             // Delete all their old answers.
-            foreach($q->answers as $a) {
+            foreach ($q->answers as $a) {
                 if ($a->selected) {
                     $DB->delete_records("teambuilder_response", array("userid" => $USER->id, "answerid" => $a->id));
                 }
             }
             // Now insert their new answers.
-            if(is_array($response)) {
-                foreach($response as $r) {
+            if (is_array($response)) {
+                foreach ($response as $r) {
                     $record = new stdClass();
                     $record->userid = $USER->id;
                     $record->answerid = $r;
-                    $DB->insert_record("teambuilder_response",$record);
+                    $DB->insert_record("teambuilder_response", $record);
                 }
             } else {
                 $record = new stdClass();
                 $record->userid = $USER->id;
                 $record->answerid = $response;
-                $DB->insert_record("teambuilder_response",$record);
+                $DB->insert_record("teambuilder_response", $record);
             }
         }
         $feedback = "Your answers were submitted.";
     }
 }
 
-/// Print the page header
-$strteambuilders = get_string('modulenameplural', 'teambuilder');
-$strteambuilder  = get_string('modulename', 'teambuilder');
-
-/// Print the main part of the page
-
 $mode = 'student';
 
-if(has_capability('mod/teambuilder:create', $ctxt)) {
-    if($preview) {
+if (has_capability('mod/teambuilder:create', $ctxt)) {
+    if ($preview) {
         $mode = 'preview';
         $PAGE->requires->js("/mod/teambuilder/js/view.js");
     } else {
@@ -107,71 +100,70 @@ if(has_capability('mod/teambuilder:create', $ctxt)) {
         $PAGE->requires->js("/mod/teambuilder/js/editview.js");
     }
 } else {
-    require_capability('mod/teambuilder:respond',$ctxt);
+    require_capability('mod/teambuilder:respond', $ctxt);
     $mode = 'student';
     $PAGE->requires->js("/mod/teambuilder/js/view.js");
 }
 
 if (($mode == 'teacher') && ($teambuilder->open < time()) && !isset($_GET['f'])) {
-    redirect("build.php?id=$id");
+    redirect(new moodle_url('/mod/teambuilder/build.php', ['id' => $id]));
 }
 
 $PAGE->set_url('/mod/teambuilder/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($teambuilder->name));
 $PAGE->set_heading($course->fullname);
-$PAGE->requires->css('/mod/teambuilder/css/custom-theme/jquery.ui.css');
 $PAGE->set_cm($cm);
 $PAGE->set_context($ctxt);
-$PAGE->set_button($OUTPUT->update_module_button($cm->id, 'teambuilder'));
-echo $OUTPUT->header();
+$output = $PAGE->get_renderer('mod_teambuilder');
+echo $output->header();
 
-//first things first: if it's not open, don't show it to students
+// First things first: if it's not open, don't show it to students.
 
-if(($mode=="student") && $teambuilder->groupid && !groups_is_member($teambuilder->groupid)) {
-    echo '<div class="ui-widget" style="text-align:center;"><div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all"><p>You do not need to complete this Team Builder questionnaire.</p></div></div>';
-} else if(($mode=="student") && (($teambuilder->open > time()) || $teambuilder->close < time())) {
-    echo '<div class="ui-widget" style="text-align:center;"><div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all"><p>This Team Builder questionnaire is not open.</p></div></div>';
+if (($mode == "student") && $teambuilder->groupid && !groups_is_member($teambuilder->groupid)) {
+    echo '<div class="ui-widget" style="text-align:center;">';
+    echo '<div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all">';
+    echo '<p>'.get_string('noneedtocomplete', 'mod_teambuilder').'</p>';
+    echo '</div></div>';
+} else if (($mode == "student") && (($teambuilder->open > time()) || $teambuilder->close < time())) {
+    echo '<div class="ui-widget" style="text-align:center;">';
+    echo '<div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all">';
+    echo '<p>'.get_string('notopen', 'mod_teambuilder').'</p>';
+    echo '</div></div>';
 } else {
-
-    if ($mode=='teacher') {
-
-        //before we start - import the questions
+    if ($mode == 'teacher') {
+        // Before we start - import the questions.
         $import = optional_param('import', 0, PARAM_INT);
-        if ($import)
-        {
+        if ($import) {
             $questions = teambuilder_get_questions($import);
-            foreach($questions as $q)
-            {
+            foreach ($questions as $q) {
                 unset($q->id);
                 $q->builder = $teambuilder->id;
-                $newid = $DB->insert_record('teambuilder_question',$q);
-                foreach($q->answers as $a)
-                {
+                $newid = $DB->insert_record('teambuilder_question', $q);
+                foreach ($q->answers as $a) {
                     unset($a->id);
                     $a->question = $newid;
-                    $DB->insert_record('teambuilder_answer',$a);
+                    $DB->insert_record('teambuilder_answer', $a);
                 }
             }
         }
 
-        $tabs = array();
-        $tabs[] = new tabobject("questionnaire","view.php?id=$id",get_string('questionnaire','teambuilder'));
-        $tabs[] = new tabobject("preview","view.php?id=$id&preview=1",get_string('preview','teambuilder'));
-        $tabs[] = new tabobject("build","build.php?id=$id",get_string('buildteams','teambuilder'));
-        print_tabs(array($tabs), "questionnaire");
+        echo $output->navigation_tabs($id, "questionnaire");
 
-        if($teambuilder->open < time())
-        {
-                echo '<div class="ui-widget" style="text-align:center;"><div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all"><p>You cannot edit the questionnaire of a Team Builder if it has already been opened.</p></div></div>';
+        if ($teambuilder->open < time()) {
+                echo '<div class="ui-widget" style="text-align:center;">';
+                $style = "display:inline-block; padding-left:10px; padding-right:10px;";
+                echo '<div style="'.$style.'" class="ui-state-highlight ui-corner-all">';
+                echo '<p>'.get_string('noeditingafteropentime', 'mod_teambuilder').'</p>';
+                echo '</div></div>';
                 echo '<script type="text/javascript">var interaction_disabled = true;</script>';
         }
 
-        //set up initial questions
+        // Set up initial questions.
         $questions = teambuilder_get_questions($teambuilder->id);
         echo '<script type="text/javascript"> var init_questions = ' . json_encode($questions) . '</script>';
 
         echo '<div id="questions">';
-        foreach($questions as $q) {
+        foreach ($questions as $q) {
             echo <<<HTML
 <div class="question" id="question-{$q->id}"><table>
 <tr>
@@ -184,8 +176,7 @@ if(($mode=="student") && $teambuilder->groupid && !groups_is_member($teambuilder
 <tr>
     <td class="answers" colspan="2"><ul>
 HTML;
-            foreach($q->answers as $a)
-            {
+            foreach ($q->answers as $a) {
                 echo "<li>$a->answer</li>";
             }
             echo  '</ul></td></tr></table></div>';
@@ -193,42 +184,52 @@ HTML;
 
         echo '</div>';
 
-        if($teambuilder->open > time()) {
+        if ($teambuilder->open > time()) {
 
             // New question form.
-            echo <<<HTML
-<div style="display:none;text-align:center;" id="savingIndicator"></div>
-<div style="text-align:center;"><button type="button" id="saveQuestionnaire" onclick="saveQuestionnaire('$CFG->wwwroot/mod/teambuilder/ajax.php',$id)">Save Questionnaire</button></div>
-HTML;
+            $onclick = "saveQuestionnaire('{$CFG->wwwroot}/mod/teambuilder/ajax.php', {$id})";
+            echo '<div style="display:none;text-align:center;" id="savingIndicator"></div>';
+            echo '<div style="text-align:center;"><button type="button" id="saveQuestionnaire" onclick="'.$onclick.'">';
+            echo get_string('savequestionnaire', 'mod_teambuilder').'</button></div>';
 
             if (empty($questions)) {
                 $otherbuilders = $DB->get_records('teambuilder', array('course' => $course->id));
-                echo '<div style="text-align:center;margin:10px;font-weight:bold;" id="importContainer">Import questions from: <select id="importer">';
-                foreach($otherbuilders as $o) {
+                $strimport = get_string('importquestionsfrom', 'mod_teambuilder');
+                echo '<div style="text-align:center;margin:10px;font-weight:bold;" id="importContainer">';
+                echo $strimport.': <select id="importer">';
+                foreach ($otherbuilders as $o) {
                     echo "<option value=\"$o->id\">$o->name</option>";
                 }
                 echo '</select><button type="button" id="importButton">Import</button><br/>OR</div>';
             }
 
+            $straddanewquestion = get_string('addanewquestion', 'mod_teambuilder');
+            $straddnewquestion = get_string('addnewquestion', 'mod_teambuilder');
+            $strquestion = get_string('question');
+            $stranswertype = get_string('answertype', 'mod_teambuilder');
+            $stranswers = get_string('answers', 'mod_teambuilder');
+            $strselectone = get_string('selectone', 'mod_teambuilder');
+            $strselectany = get_string('selectany', 'mod_teambuilder');
+            $strselectatleastone = get_string('selectatleastone', 'mod_teambuilder');
             echo <<<HTML
-<div style="text-align:center;font-weight:bold;margin:10px;">Add a new question</div>
+<div style="text-align:center;font-weight:bold;margin:10px;">$straddanewquestion</div>
 <div style="text-align:center;">
 <div id="newQuestionForm">
     <table>
         <tr>
-            <th scope="row">Question</th>
+            <th scope="row">$strquestion</th>
             <td><input name="question" type="text" class="text" /></td>
         </tr>
         <tr>
-            <th scope="row">Answer type</th>
+            <th scope="row">$stranswertype</th>
             <td><select>
-                <option value="one">Select one</option>
-                <option value="any">Select any (or none)</option>
-                <option value="atleastone">Select one or more</option>
+                <option value="one">$strselectone</option>
+                <option value="any">$strselectany</option>
+                <option value="atleastone">$strselectatleastone</option>
             </select></td>
         </tr>
         <tr>
-            <th scope="row">Answers</th>
+            <th scope="row">$stranswers</th>
             <td id="answerSection"><input type="text" name="answers[]" class="text" /><br/>
                 <button onclick="addNewAnswer();" type="button">+</button>
                 <button onclick="removeLastAnswer();" type="button">-</button>
@@ -236,33 +237,33 @@ HTML;
         </tr>
         <tr>
             <td></td>
-            <td><button id="addNewQuestion" type="button" onclick="addNewQuestion();">Add New Question</button></td>
+            <td><button id="addNewQuestion" type="button" onclick="addNewQuestion();">$straddnewquestion</button></td>
         </tr>
     </table>
 </div>
 </div>
 HTML;
         }
-    } else if (($mode=="preview") || ($mode=="student")) {
-        $questions = teambuilder_get_questions($teambuilder->id,$USER->id);
-        $responses = teambuilder_get_responses($teambuilder->id,$USER->id);
+    } else if (($mode == "preview") || ($mode == "student")) {
+        $questions = teambuilder_get_questions($teambuilder->id, $USER->id);
+        $responses = teambuilder_get_responses($teambuilder->id, $USER->id);
 
         if ($mode == "preview") {
-            $tabs = array();
-            $tabs[] = new tabobject("questionnaire","view.php?id=$id&f=1",get_string('questionnaire','teambuilder'));
-            $tabs[] = new tabobject("preview","view.php?id=$id&preview=1",get_string('preview','teambuilder'));
-            $tabs[] = new tabobject("build","build.php?id=$id",get_string('buildteams','teambuilder'));
-            print_tabs(array($tabs), "preview");
+            echo $output->navigation_tabs($id, "preview");
         }
 
         if (($mode == "student") && empty($feedback)) {
-            if($responses!==false && !$teambuilder->allowupdate) {
+            if ($responses !== false && !$teambuilder->allowupdate) {
                 $feedback = "You have already completed this questionnaire.";
             }
         }
 
         if (isset($feedback) && $feedback) {
-            echo '<div class="ui-widget" style="text-align:center;"><div style="display:inline-block; padding-left:10px; padding-right:10px;" class="ui-state-highlight ui-corner-all"><p>'.$feedback.'</p></div></div>';
+            echo '<div class="ui-widget centered">';
+            $style = 'display:inline-block; padding-left:10px; padding-right:10px;';
+            echo '<div style="'.$style.'" class="ui-state-highlight ui-corner-all">';
+            echo '<p>'.$feedback.'</p>';
+            echo '</div></div>';
         }
 
         if (!empty($teambuilder->intro)) {
@@ -273,8 +274,12 @@ HTML;
             $preview = $mode == "preview" ? "&preview=1" : "";
             echo '<form onsubmit="return validateForm(this)" action="view.php?id='.$id.$preview.'" method="POST">';
 
-            $displaytypes = array("one" => "Select <strong>one</strong> of the following:", "any" => "Select any (or none) of the following:", "atleastone" => "Select <strong>at least one</strong> of the following:");
-            foreach($questions as $q) {
+            $displaytypes = [
+                "one" => "Select <strong>one</strong> of the following:",
+                "any" => "Select any (or none) of the following:",
+                "atleastone" => "Select <strong>at least one</strong> of the following:",
+            ];
+            foreach ($questions as $q) {
                 echo <<<HTML
 <div class="question" id="question-{$q->id}"><table>
 <tr>
@@ -284,7 +289,7 @@ HTML;
     <td class="answers" colspan="2">
         <div style="visibility:hidden;">
 HTML;
-                foreach($q->answers as $a) {
+                foreach ($q->answers as $a) {
                     if ($q->type == "one") {
                         $type = "radio";
                         $name = '';
@@ -294,7 +299,9 @@ HTML;
                     }
                     $checked = $a->selected ? 'checked="checked"' : "";
                     $class = $q->type == "atleastone" ? 'class="atleastone"' : "";
-                    echo "<label><input type=\"$type\" name=\"question-$q->id$name\" value=\"$a->id\" $class $checked />$a->answer</label>";
+                    $input = html_writer::empty_tag('input',
+                        ['type' => $type, 'name' => "question-{$q->id}{$name}", 'value' => $a->id]);
+                    echo html_writer::label($input.$a->answer, null);
                 }
                 echo <<<HTML
         </div>
@@ -312,8 +319,5 @@ HTML;
 
         }
     }
-
-} // If student and outside of open/close.
-
-/// Finish the page
+}
 echo $OUTPUT->footer();
