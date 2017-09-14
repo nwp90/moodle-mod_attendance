6ed8b19750f763f1150e866ae9ef4faa47a26323
Nick Phillips <nick.phillips@otago.ac.nz>
2017-09-14 14:45:40 +1200

    Add medschool_topics format.

---
+++ course/format/medschool_topics/lang/en/format_medschool_topics.php
@@ -0,0 +1,53 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_medschool_topics', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_medschool_topics
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//global $CFG;
+//require_once('../../../config.php');
+
+global $COURSE;
+
+
+$string['currentsection'] = 'This topic';
+$string['editsection'] = 'Edit topic';
+$string['editsectionname'] = 'Edit topic name';
+$string['deletesection'] = 'Delete topic';
+$string['newsectionname'] = 'New name for topic {$a}';
+$string['sectionname'] = 'Topic';
+$string['pluginname'] = 'Medschool Topics format';
+$string['section0name'] = 'Welcome to {$a}';
+$string['section1name'] = 'Course Information';
+$string['section2name'] = 'Assessment';
+$string['page-course-view-topics'] = 'Any course main page in topics format';
+$string['page-course-view-topics-x'] = 'Any course page in topics format';
+$string['hidefromothers'] = 'Hide topic';
+$string['showfromothers'] = 'Show topic';
+$string['sectionicon'] = 'Section Icon';
+$string['navname'] = 'Navigation title';
+$string['navname_help'] = 'This shortened version of the section name will appear in your navigation bar under the icon for this section. <br /><br />For more help see the Moodle Helpsite at <a href="https://help.otago.ac.nz/moodle/building-your-course/medschool-topics-course-format/">https://help.otago.ac.nz/moodle</a>';
+$string['sectionicon_help'] = 'This icon will be used in the navigation bar at the top of the course. <br /><br />You can find more help with these icons on the Moodle Helpsite at <a href="https://help.otago.ac.nz/moodle/building-your-course/medschool-topics-course-format/">https://help.otago.ac.nz/moodle</a>';
+$string['coursebannerdisplay'] = 'Display the course banner';
+$string['coursebannerdisplay_help'] = 'This will show an image at the top of your course. For custom images upload a file named Header.jpg to the Course summary files above. <br /><br />For more help see the Moodle Helpsite at <a href="https://help.otago.ac.nz/moodle/building-your-course/medschool-topics-course-format/">https://help.otago.ac.nz/moodle</a>';
+$string['navigationbardisplay'] = 'Display the navigation bar';
+$string['navigationbardisplay_help'] = 'This will create an automatic navigation bar for your course with customisable icons. Each visible section in your course will appear in the navigation bar. <br /><br />For more help see the Moodle Helpsite at <a href="https://help.otago.ac.nz/moodle/building-your-course/medschool-topics-course-format/">https://help.otago.ac.nz/moodle</a>';
