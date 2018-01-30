445e292e601222ad322d042b2e0192f64dba969b
Nick Phillips <nick.phillips@otago.ac.nz>
2018-01-31 08:19:05 +1300

    Revert "MDL-60402 calendar: check enrolment at last chance in container"
    This reverts commit f2152ce5034ce545233e523bd7726fe6af7141e7.

--- calendar/classes/local/event/container.php
+++ calendar/classes/local/event/container.php
@@ -151,16 +151,11 @@ class container {
                     // 2) Only process modules for courses a user has the capability to view OR they are enrolled in.
                     // 3) Only process modules for courses that are visible OR if the course is not visible, the user
                     //    has the capability to view hidden courses.
-                    if (!$cm->uservisible) {
-                        return true;
-                    }
-
                     $coursecontext = \context_course::instance($dbrow->courseid);
-                    if (!$cm->get_course()->visible && !has_capability('moodle/course:viewhiddencourses', $coursecontext)) {
-                        return true;
-                    }
-
-                    if (!has_capability('moodle/course:view', $coursecontext) && !is_enrolled($coursecontext)) {
+                    $canseecourse = has_capability('moodle/course:view', $coursecontext) || is_enrolled($coursecontext);
+                    $canseecourse = $canseecourse &&
+                        ($cm->get_course()->visible || has_capability('moodle/course:viewhiddencourses', $coursecontext));
+                    if (!$cm->uservisible || !$canseecourse) {
                         return true;
                     }
 
