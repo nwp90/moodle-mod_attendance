01f390e489952e740f82812bda5c1b3d1c481752
Nick Phillips <nick.phillips@otago.ac.nz>
2018-01-31 08:18:55 +1300

    Revert "MDL-60402 external: contexts can be passed to external format funcs"
    This reverts commit 85b485c51922ca84e715f6d1a24d05f0f155a995.

--- lib/classes/external/exporter.php
+++ lib/classes/external/exporter.php
@@ -157,7 +157,7 @@ abstract class exporter {
                 $formatparams = $this->get_format_parameters($property);
                 $format = $record->$propertyformat;
 
-                list($text, $format) = external_format_text($data->$property, $format, $formatparams['context'],
+                list($text, $format) = external_format_text($data->$property, $format, $formatparams['context']->id,
                     $formatparams['component'], $formatparams['filearea'], $formatparams['itemid'], $formatparams['options']);
 
                 $data->$property = $text;
@@ -168,11 +168,11 @@ abstract class exporter {
 
                 if (!empty($definition['multiple'])) {
                     foreach ($data->$property as $key => $value) {
-                        $data->{$property}[$key] = external_format_string($value, $formatparams['context'],
+                        $data->{$property}[$key] = external_format_string($value, $formatparams['context']->id,
                             $formatparams['striplinks'], $formatparams['options']);
                     }
                 } else {
-                    $data->$property = external_format_string($data->$property, $formatparams['context'],
+                    $data->$property = external_format_string($data->$property, $formatparams['context']->id,
                             $formatparams['striplinks'], $formatparams['options']);
                 }
             }
