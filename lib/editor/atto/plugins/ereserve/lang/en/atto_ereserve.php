93a25159c0c184e42c991d2f1e14f2cf62de507d
Nick Phillips <nick.phillips@otago.ac.nz>
2018-04-13 14:16:52 +1200

    Add ereserve atto plugin

---
+++ lib/editor/atto/plugins/ereserve/lang/en/atto_ereserve.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_ereserve', language 'en'.
+ *
+ * @package    atto_ereserve
+ * @copyright  2018 eReserve Pty Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['createereserve'] = 'Create eReserve Plus Resource Link';
+$string['pluginname'] = 'eReserve Plus Resource Linking';
+$string['settings'] = 'eReserve Plus Resource Linking';
+$string['browse'] = 'Browse';
+$string['visible'] = 'Visible';
+$string['nothingtoinsert'] = 'Nothing to insert!';
+$string['dialogtitle'] = 'Create eReserve Plus resource link';
+$string['buttontitle'] = 'Create eReserve Plus resource link';
+$string['insert'] = 'Insert';
+$string['cancel'] = 'Cancel';
+$string['ereserve:visible'] = 'Visible';
