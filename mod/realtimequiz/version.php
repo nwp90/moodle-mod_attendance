50335b7eb1c4fe1895a0c724089218d770b83b15
Nick Phillips <nick.phillips@otago.ac.nz>
2018-07-03 13:30:07 +1200

    Update mod realtimequiz to r3.4+/2018042100

--- mod/realtimequiz/version.php
+++ mod/realtimequiz/version.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Code fragment to define the version of realtimequiz
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
@@ -8,24 +23,9 @@
  **/
 
 defined('MOODLE_INTERNAL') || die();
-global $CFG;
 
-if (!isset($plugin)) {
-    $plugin = new stdClass(); // Avoid warnings in Moodle 2.5 and below.
-}
-
-$plugin->version = 2017110900;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2010112400;  // Moodle 2.0 (or above).
-$plugin->cron = 0;           // Period for cron to check this module (secs).
+$plugin->version = 2018042100;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2017111300;  // Moodle 3.4 (or above).
 $plugin->component = 'mod_realtimequiz';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.x (Build: 2017110900)';
-
-if (isset($CFG->branch) && $CFG->branch < 26) {
-    $module->version = $plugin->version;
-    $module->requires = $plugin->requires;
-    $module->cron = $plugin->cron;
-    $module->component = $plugin->component;
-    $module->maturity = $plugin->maturity;
-    $module->release = $plugin->release;
-}
+$plugin->release = '3.4+ (Build: 2018042100)';
